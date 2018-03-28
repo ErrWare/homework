9821c67b07eb6a9c29f2e1d0b8613e9a24215cbe
ErrWare <ilan.buzzetti@gmail.com>
2018-03-27 21:34:28 -0500

    Finished the basic setup of the project, ready to make threads

--- operatingsystems/OSProj2/proj.cpp
+++ operatingsystems/OSProj2/proj.cpp
@@ -12,6 +12,24 @@
 #include <sys/types.h>
 
 void* print_thread(void*);
+void* patient(void*);
+void* nurse(void*);
+void* receptionist(void*);
+void* dr(void*);
+
+sem_t waiting_room_door;
+sem_t waiting_room;
+sem_t receptionist_admit;
+sem_t receptionist_assigned;
+sem_t patient_accept;
+sem_t nurses[3];
+sem_t drs[3];
+sem_t entry[3];
+sem_t ready2gab[3];
+sem_t advice_given[3];
+sem_t assignedQ[3];
+
+int* patient_to_dr;
 
 int main(int argc, char *argv[])
 {
@@ -24,17 +42,111 @@ int main(int argc, char *argv[])
     
     int doctors = atoi(argv[1]);
     int patients = atoi(argv[2]);
+    patient_to_dr = new int[patients];
 
-    std::cout << "Drs: " << doctors << "\tpatients: " << patients << "\tDrs * patients: " << doctors * patients << std::endl;
+
+    if (sem_init (&waiting_room_door, 0, 1) == -1) exit(1);
+    if (sem_init (&waiting_room, 0, 0) == -1) exit(1);
+    if (sem_init (&receptionist_assigned, 0, 0) == -1) exit(1);
+    if (sem_init (&patient_accept, 0, 0) == -1) exit(1);
+    //create an array of literals because the pthread create pass by reference
+    //is liable to have the val at payload address change before it is used
+    int literals[] = {0,1,2};
+    for(int i = 0; i<doctors; i++){
+        if (sem_init (&(nurses[i]), 0, 0) == -1) exit(1);
+        if (sem_init (&(drs[i]), 0, 0) == -1) exit(1);
+        if (sem_init (&(assignedQ[i]), 0, 0) == -1) exit(1);
+        if (sem_init (&(entry[i]), 0, 0) == -1) exit(1);
+        if (sem_init (&(ready2gab[i]), 0, 0) == -1) exit(1);
+        if (sem_init (&(advice_given[i]), 0, 0) == -1) exit(1);
+        pthread_t tid1;
+        pthread_t tid2;
+        pthread_create(&tid1, NULL, dr, literals+i);
+        pthread_create(&tid2, NULL, nurse, literals+i);
+    }
+
+
+
+//    std::cout << "Drs: " << doctors << "\tpatients: " << patients << "\tDrs * patients: " << doctors * patients << std::endl;
 
     pthread_t tid;
     int i = 7;
     pthread_create(&tid, NULL, print_thread, &i);
 
     pthread_join(tid, NULL);
+
+    sleep(patients);
+
 }
 
 void* print_thread(void* input){
-    printf("%d\n", 5 * *((int*)input));
+  //  printf("%d\n", 5 * *((int*)input));
     return NULL;
+}
+
+void* patient(void* input){
+
+    return NULL;
+}
+void* receptionist(void* input){
+
+    return NULL;
+}
+void* nurse(void* input){
+    printf("nurse %d create\n", *((int*)input));
+    return NULL;
+}
+void* dr(void* input){
+    printf("dr %d created\n", *((int*)input));
+    return NULL;
+}
+
+class Queue {
+    class Node{
+        Node* next;
+        int i;
+        public:
+        Node(int j){
+            i = j;
+            next = NULL;
+        }
+        void setNext(Node* n){
+            next = n;
+        }
+        Node* getNext(){
+            return next;
+        }
+        int getNum(){
+            return i;
+        }
+        Node* insert(int j){
+            next = new Node(j);
+            return next;
+        }
+    }
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    public:
+        bool isEmpty(){
+            return head == NULL;
+        }
+        int peek(){
+            return (*head).getNum();
+        }
+        int poll(){
+            int i = peek();
+            Node* n = (*head).getNext();
+            delete(head);
+            head = n;
+        }
+        void insert(int i){
+            if(isEmpty()){
+                head = new Node(i);
+                tail = head;
+            } else {
+                tail = (*tail).insert(i);
+            }
+        }
+
 }

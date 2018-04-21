fdeb831995c00adfe8b2bc4b2347865de0e569b2
ErrWare <ilan.buzzetti@gmail.com>
2018-04-21 15:14:54 -0500

    Done with all but feedback

--- operatingsystems/OSProj3/schedule_sim.cpp
+++ operatingsystems/OSProj3/schedule_sim.cpp
@@ -25,7 +25,7 @@ class Job{
         id = total_jobs++;
     }
     int remaining_time(){return total_time - serviced_time;}
-
+    double approx_RR(int t){return ((double)(t - arrival_time + total_time))/total_time;}
     void print(){
         std::cout << id << " : " << arrival_time << "\t" << total_time << std::endl;
     }
@@ -73,17 +73,73 @@ void RR(){
     jq.push(jobs[0]);
     Job* j;
     while(time < total_work_time){
+        //pop job from RR queue and service
         *j = jq.front();
         jq.pop();
         serviceJob(time, *j);
+        //enqueue any newly arrived jobs
         for(int i = min_job_not_in_queue; i < Job::total_jobs; i++){
             if(jobs[i].arrival_time <= time){
                 jq.push(jobs[i]);
                 min_job_not_in_queue++;
             }
         }
+        //push serviced job back into queue if needs more time
         if(j->remaining_time() > 0)
             jq.push(*j);
+    }
+}
+void SRT(){
+    int time = 0;
+    while(time < total_work_time){
+        //Choose next job by SRT metric
+        int jobid = 0;
+        int min = total_work_time + 1;
+        for(int i = 0; i < Job::total_jobs; i++){
+            if(jobs[i].arrival_time <= time && jobs[i].remaining_time() < min && jobs[i].remaining_time() > 0){
+                min = jobs[i].remaining_time();
+                jobid = i;
+            }
+        }
+        //Service job one time step
+        serviceJob(time, jobs[jobid]);
+    }
+}
+void SPN(){
+    int time = 0;
+    while(time < total_work_time){
+        //Choose next job by SPN metric
+        int jobid = 0;
+        int min = 9999999;
+        for(int i = 0; i < Job::total_jobs; i++){
+            if(jobs[i].arrival_time <= time && jobs[i].remaining_time() < min && jobs[i].remaining_time() > 0){
+                min = jobs[i].remaining_time();
+                jobid = i;
+            }
+        }
+        //Service job to completion
+        for(int i = 0; i < jobs[jobid].total_time; i++){
+            serviceJob(time, jobs[jobid]);
+        }
+    }
+}
+void HRRN(){
+    int time = 0;
+    while(time < total_work_time){
+        //Choose next job by HRRN metric
+        int jobid = 0;
+        double max = -1;
+        for(int i = 0; i < Job::total_jobs; i++){
+            if(jobs[i].arrival_time <= time && jobs[i].approx_RR(time) > max && jobs[i].remaining_time() > 0){
+                jobid = i;
+                max = jobs[i].approx_RR(time);
+            }
+        }
+
+        //Service job to completion
+        for(int i = 0; i < jobs[jobid].total_time; i++){
+            serviceJob(time, jobs[jobid]);
+        }
     }
 }
 
@@ -107,12 +163,24 @@ int main(int argc, char *argv[]){
     std::cout << "Total time : " << total_work_time << std::endl;
     schedule = new int[total_work_time];
     
+    std::cout << "FCFS" << std::endl;
     FCFS();
     printschedule();
-    std::cout << "resetting" << std::endl;
     resetJobs();
-    std::cout << "reset" << std::endl;
+    std::cout << "RR" << std::endl;
     RR();
+    printschedule();
+    resetJobs();
+    std::cout << "SRT" << std::endl;
+    SRT();
+    printschedule();
+    resetJobs();
+    std::cout << "SPN" << std::endl;
+    SPN();
+    printschedule();
+    resetJobs();
+    std::cout << "HRRN" << std::endl;
+    HRRN();
     printschedule();
     //ifs.close();
 
